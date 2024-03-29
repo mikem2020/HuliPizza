53a3e9f923d5a1752f666e923a57f9f7bbf3a433
Michael Moll <michael_moll@outlook.com>
2024-03-29 13:07:30 -0500

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Michael Moll on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
